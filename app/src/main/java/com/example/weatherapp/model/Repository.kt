da501855ba4cd3f048cf0a103813ce13b2c83b0e
maksimsemenakin <tvpilip@gmail.com>
2022-01-31 22:14:39 +0300

    lesson5 - добавил интеграцию с api яндекс.погоды через HttpsUrl

--- app/src/main/java/com/example/weatherapp/model/Repository.kt
+++ app/src/main/java/com/example/weatherapp/model/Repository.kt
@@ -3,7 +3,7 @@ package com.example.weatherapp.model
 class Repository : RepositoryImpl {
 
     override fun getWeather(): CityInfo =
-        CityInfo(City("Krasnodar", 3), 2)
+        CityInfo(City("Krasnodar",54.7387621,55.972055400000045), 2)
 
     override fun getWeatherFromLocalStorageRus() =  getRussianCities()
 
